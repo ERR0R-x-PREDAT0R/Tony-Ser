085c9896fd1662eef408a31a264b9ec92508365a
ALINSHAN <92071951+ERR0R-x-PREDAT0R@users.noreply.github.com>
2022-03-04 17:55:05 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *Tony Ser*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *Tony Ser*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *Tony Ser*:  ```" + msg + "```"
 }
 
 
